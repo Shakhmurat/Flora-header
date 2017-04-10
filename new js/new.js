c4d0f8c694e02767e5ad90ca4ec23f51c152ab3e
RocketMan <rocketman@Shaaa.local>
2017-04-10 12:59:48 +0600

    added new js

--- new js/new.js
+++ new js/new.js
@@ -12,3 +12,18 @@ $('#search-submit').click(function (e) {
 
 });
 
+// Если браузер не поддерживает свойство object-fit 
+if ('objectFit' in document.documentElement.style === false) {
+  var container = document.getElementsByClassName('fit-img-js');
+  for (var i = 0; i < container.length; i++) {
+    var imageSource = container[i].querySelector('img').src;
+    container[i].querySelector('img').style.opacity = '0';
+    container[i].style.backgroundSize = 'cover';
+    container[i].style.backgroundImage = 'url(' + imageSource + ')';
+    container[i].style.backgroundPosition = 'center center';
+    container[i].style.backgroundRepeat = 'no-repeat';
+  }
+} else {
+    console.log('No worries, your browser supports objectFit');
+}
+
